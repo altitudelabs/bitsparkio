490342612703f52917256d1716bf1cd39cfa11e5
Eugene Choi <eugene.maestro.choi@gmail.com>
2014-06-11 13:37:37 +0800

    added env.port in server.js

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var connect = require('connect');
 process.setMaxListeners(0);
 connect()
     .use(connect.static('./'))
-    .listen(8000);
+    .listen(process.env.PORT || 8000);
 
 console.log('Listening on port 8000.');
